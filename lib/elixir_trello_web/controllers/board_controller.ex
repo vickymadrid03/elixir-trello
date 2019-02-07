3b46ed75460cefcd5e1fdaf6c9c38970cafde5b9
Victoria Madrid <victoria@wyeworks.com>
2019-02-07 16:30:44 -0300

    migration to create boards

---
+++ lib/elixir_trello_web/controllers/board_controller.ex
@@ -0,0 +1,43 @@
+defmodule ElixirTrelloWeb.BoardController do
+  use ElixirTrelloWeb, :controller
+
+  alias ElixirTrello.Boards
+  alias ElixirTrello.Boards.Board
+
+  action_fallback ElixirTrelloWeb.FallbackController
+
+  def index(conn, _params) do
+    boards = Boards.list_boards()
+    render(conn, "index.json", boards: boards)
+  end
+
+  def create(conn, %{"board" => board_params}) do
+    with {:ok, %Board{} = board} <- Boards.create_board(board_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.board_path(conn, :show, board))
+      |> render("show.json", board: board)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    board = Boards.get_board!(id)
+    render(conn, "show.json", board: board)
+  end
+
+  def update(conn, %{"id" => id, "board" => board_params}) do
+    board = Boards.get_board!(id)
+
+    with {:ok, %Board{} = board} <- Boards.update_board(board, board_params) do
+      render(conn, "show.json", board: board)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    board = Boards.get_board!(id)
+
+    with {:ok, %Board{}} <- Boards.delete_board(board) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
---
+++ lib/elixir_trello_web/views/board_view.ex
@@ -0,0 +1,17 @@
+defmodule ElixirTrelloWeb.BoardView do
+  use ElixirTrelloWeb, :view
+  alias ElixirTrelloWeb.BoardView
+
+  def render("index.json", %{boards: boards}) do
+    %{data: render_many(boards, BoardView, "board.json")}
+  end
+
+  def render("show.json", %{board: board}) do
+    %{data: render_one(board, BoardView, "board.json")}
+  end
+
+  def render("board.json", %{board: board}) do
+    %{id: board.id,
+      name: board.name}
+  end
+end
